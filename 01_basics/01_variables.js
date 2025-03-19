22c1e69b1252556a1a00cfbda21c8f14e8a1b521
Muhammad Amjad <amjad.venv@gmail.com>
2025-03-19 13:17:13 +0000

    console.table and variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const roll_no = 20
+let email = "student@google.com"
+// do not use var (variable declaration).
+// because of functional scope and 
+var name = "Student Name"
+city = "Student city"
+
+console.log(roll_no)
+console.log(email)
+console.log(name)
+console.log(city)
+
+console.table([roll_no, name, city, email])
