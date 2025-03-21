4e538965eddc850a1f31de4998120b1c622ab7c4
Muhammad Amjad <amjad.venv@gmail.com>
2025-03-21 23:57:56 +0500

    Strings and their concatination

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,18 @@
+// Strins are combination of characters
+// We can define strings by using single quotes double quotes and backticks(``)
+
+let singleQuoteString = 'Hi'
+let doubleQuoteString = "Hello"
+let backticksString = `hey` // Backticks allow you to embed expressions (variables or operations)
+
+// --> Strings Concatination
+
+let greeting = "Hello"
+let nam = 'Amjad'
+let message = greeting + " " + name + "!"
+console.log(message)
+
+// By Using Tempelate Literals (Backticks)
+
+message = `${greeting} ${name}!`
+console.log(message)
