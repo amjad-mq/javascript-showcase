efd21f24c63a0f5ed59f149225b670b145300d84
Muhammad Amjad <amjad.venv@gmail.com>
2025-04-14 09:14:24 +0000

    Object use case

---
+++ 03_Object/05_storing_user_data.js
@@ -0,0 +1,9 @@
+// Common in web forms
+
+const user = {
+    id: 101,
+    name: "Ali Aslam",
+    email: "alisemail@gamil.com",
+    cartItems: ["Laptop", "Mouse", "Keyborad"],
+    isPremium: false
+}
