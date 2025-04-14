0e0f4e347ec2c3349b757c8069adb563e2db3178
Muhammad Amjad <amjad.venv@gmail.com>
2025-04-14 07:41:05 +0000

    Array Concatination

---
+++ 02_Array/02_concat_array_in_java.js
@@ -0,0 +1,4 @@
+let first = [1, 2, 3]
+let second = [11, 22, 33]
+let combine = first.concat(second)
+console.log(combine)
---
+++ 02_Array/03_array_spread_operator.js
@@ -0,0 +1,4 @@
+let array1 = [1, 2, 3, "Amjad", 3.67]
+let array2 = [ "0", 34, 56, "second", 3.67]
+let combine =[...array1 ,...array2]
+console.log(combine)
